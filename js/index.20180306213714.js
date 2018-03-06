e6d1f2af307e84da7de121ce6195a2536b210f73
daooshee <daooshee@qq.com>
2018-03-06 21:39:29 +0800

    edit

---
+++ js/index.20180306213714.js
@@ -0,0 +1,13 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+
+$(document).ready(function() {
+r=function(){dpi=window.devicePixelRatio;$('.js').attr('src', (dpi>1) ? 'images/fg-1478.jpg' : 'images/fg-739.jpg');
+$('.js-2').attr('src', (dpi>1) ? 'images/result-1532.jpg' : 'images/result-766.jpg');
+$('.js-3').attr('src', (dpi>1) ? 'images/app1-1482.jpg' : 'images/app1-741.jpg');
+$('.js-4').attr('src', (dpi>1) ? 'images/app2-1482.jpg' : 'images/app2-741.jpg');};
+if(!window.HTMLPictureElement){r();}
+(function(){$('a[href^="#"]').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+
+});
